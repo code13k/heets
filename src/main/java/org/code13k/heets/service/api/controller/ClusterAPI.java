ef4b42c62f554bc47ea63b0ae0be322ce12f4645
WikiDru <wikidru@gmail.com>
2018-10-05 02:27:01 +0900

    [Release] 1.0.0-Alpha.1

--- src/main/java/org/code13k/heets/service/api/controller/ClusterAPI.java
+++ src/main/java/org/code13k/heets/service/api/controller/ClusterAPI.java
@@ -4,7 +4,7 @@ import org.code13k.heets.app.Cluster;
 
 public class ClusterAPI extends BasicAPI {
     /**
-     * Environment
+     * Status
      */
     public String status() {
         return toResultJsonString(Cluster.getInstance().values());
